d9ca72bb386fabbbe9cd36e8006b15b07e0cb920
Immow <37532647+Immow@users.noreply.github.com>
2021-07-06 17:10:30 +0200

    Update BuildAndroid.ps1

--- BuildAndroid.ps1
+++ BuildAndroid.ps1
@@ -1,11 +1,15 @@
 $file = "C:\Dev\love-android\app\src\main\assets\game.love"
 $file2 = "C:\Dev\love-android\app\build\outputs\apk\normal\debug\app-normal-debug.apk"
+$gameSource = "D:\Documents\Programming\Lua\Games\Numbergame\*"
+$assetsPath = "C:\Dev\love-android\app\src\main\assets\game.zip"
+$loveAndroidPath = "C:\Dev\love-android"
+
 if (-not(Test-Path -Path $file -PathType Leaf)) {
      try {
       Remove-Item $file2
-      Compress-Archive -Path D:\Documents\Programming\Lua\Games\Numbergame\* -DestinationPath C:\Dev\love-android\app\src\main\assets\game.zip
-      Rename-Item -Path "C:\Dev\love-android\app\src\main\assets\game.zip" -NewName "game.love"
-      Set-Location -Path "C:\Dev\love-android"
+      Compress-Archive -Path $gameSource -DestinationPath $assetsPath
+      Rename-Item -Path $assetsPath -NewName "game.love"
+      Set-Location -Path $loveAndroidPath
       ./gradlew assembleNormal
      }
      catch {
@@ -15,8 +19,8 @@ if (-not(Test-Path -Path $file -PathType Leaf)) {
  else {
    Remove-Item $file
    Remove-Item $file2
-   Compress-Archive -Path D:\Documents\Programming\Lua\Games\Numbergame\* -DestinationPath C:\Dev\love-android\app\src\main\assets\game.zip
-   Rename-Item -Path "C:\Dev\love-android\app\src\main\assets\game.zip" -NewName "game.love"
-   Set-Location -Path "C:\Dev\love-android"
+   Compress-Archive -Path $gameSource -DestinationPath $assetsPath
+   Rename-Item -Path $assetsPath -NewName "game.love"
+   Set-Location -Path $loveAndroidPath
    ./gradlew assembleNormal
  }
