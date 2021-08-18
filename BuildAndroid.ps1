554c941600fcf8b7a15353d01688a76dc24168b8
Immow <k.schippers@gmail.com>
2021-08-18 14:48:54 +0200

    Optimized the code

--- BuildAndroid.ps1
+++ BuildAndroid.ps1
@@ -1,32 +1,16 @@
 $gameSource = "D:\Documents\Programming\Lua\Games\Numbergame\*"
 $loveAndroidPath = "C:\Dev\love-android"
 
- if (Test-Path -Path "$loveAndroidPath\app\src\main\assets\game.love" -PathType Leaf) {
-   try {
-    Remove-Item "$loveAndroidPath\app\src\main\assets\game.love"
-   }
-   catch {
-    throw $_.Exception.Message
-  }
+Function Remove-GameFile {
+    param ($Path)
+    if (Test-Path -Path $Path -PathType Leaf) {
+        Remove-Item $Path
+    }
 }
 
- if (Test-Path -Path "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk" -PathType Leaf) {
-   try {
-    Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk"
-   }
-   catch {
-    throw $_.Exception.Message
-  }
-}
-
- if (Test-Path -Path "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk" -PathType Leaf) {
-   try {
-    Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk"
-   }
-   catch {
-    throw $_.Exception.Message
-  }
-}
+Remove-GameFile "$loveAndroidPath\app\src\main\assets\game.love"
+Remove-GameFile "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk"
+Remove-GameFile "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk"
 
 Compress-Archive -Path $gameSource -DestinationPath "$loveAndroidPath\app\src\main\assets\game.zip"
 Rename-Item -Path "$loveAndroidPath\app\src\main\assets\game.zip" -NewName "game.love"
