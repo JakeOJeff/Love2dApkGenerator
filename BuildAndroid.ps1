ee7591d7f345c92c180cc13af972a43964066d63
Immow <k.schippers@gmail.com>
2021-08-18 14:20:43 +0200

    Updated

--- BuildAndroid.ps1
+++ BuildAndroid.ps1
@@ -1,25 +1,34 @@
 $gameSource = "D:\Documents\Programming\Lua\Games\Numbergame\*"
 $loveAndroidPath = "C:\Dev\love-android"
 
-if (-not(Test-Path -Path "$loveAndroidPath\app\src\main\assets\game.love" -PathType Leaf)) {
-     try {
-      Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk"
-      Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk"
-      Compress-Archive -Path $gameSource -DestinationPath "$loveAndroidPath\app\src\main\assets\game.zip"
-      Rename-Item -Path "$loveAndroidPath\app\src\main\assets\game.zip" -NewName "game.love"
-      Set-Location -Path $loveAndroidPath
-      ./gradlew assembleEmbed
-     }
-     catch {
-         throw $_.Exception.Message
-     }
- }
- else {
-   Remove-Item "$loveAndroidPath\app\src\main\assets\game.love"
-   Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk"
-   Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk"
-   Compress-Archive -Path $gameSource -DestinationPath "$loveAndroidPath\app\src\main\assets\game.zip"
-   Rename-Item -Path "$loveAndroidPath\app\src\main\assets\game.zip" -NewName "game.love"
-   Set-Location -Path $loveAndroidPath
-   ./gradlew assembleEmbed
- }
+ if (Test-Path -Path "$loveAndroidPath\app\src\main\assets\game.love" -PathType Leaf) {
+   try {
+    Remove-Item "$loveAndroidPath\app\src\main\assets\game.love"
+   }
+   catch {
+    throw $_.Exception.Message
+  }
+}
+
+ if (Test-Path -Path "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk" -PathType Leaf) {
+   try {
+    Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\debug\app-embed-debug.apk"
+   }
+   catch {
+    throw $_.Exception.Message
+  }
+}
+
+ if (Test-Path -Path "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk" -PathType Leaf) {
+   try {
+    Remove-Item "$loveAndroidPath\app\build\outputs\apk\embed\release\app-embed-release-unsigned.apk"
+   }
+   catch {
+    throw $_.Exception.Message
+  }
+}
+
+Compress-Archive -Path $gameSource -DestinationPath "$loveAndroidPath\app\src\main\assets\game.zip"
+Rename-Item -Path "$loveAndroidPath\app\src\main\assets\game.zip" -NewName "game.love"
+Set-Location -Path $loveAndroidPath
+./gradlew assembleEmbed
